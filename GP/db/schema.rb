2238b47154e4fe345f500855e2b637c40cb956f4
mirrete <mertloka@hotmail.com>
2015-05-26 18:19:26 +0300

    solarpanels

--- GP/db/schema.rb
+++ GP/db/schema.rb
@@ -11,9 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150526131213) do
-
 
   create_table "categories", force: :cascade do |t|
     t.string   "category",   limit: 255
