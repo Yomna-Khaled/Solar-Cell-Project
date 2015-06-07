d6253284e87b01cf657ee26c4050d537f0efce7e
mayada <mayadafahim32@gmail.com>
2015-06-07 18:10:01 +0300

    mayada work

--- GP/app/assets/javascripts/material.js
+++ GP/app/assets/javascripts/material.js
@@ -148,7 +148,7 @@ function propertyajax () {
 			append('<tr><td><input type="checkbox" id="'+propertyid+
 			'" name="propertycheck[]" onchange="checkproperty(this)" value='+propertyid+'/>'
 			+propertyname+'</td><td id="value_'+propertyid+
-			'" style="display:none" onkeyup="propertyvalue(this)" placeholder="Enter Value..."><h3 style="margin-left:100px;">value</h3><input type="text" style="width:100px;margin-left:100px;" id="'+propertyid+'_value" ></td></tr>');
+			'" style="display:none" onkeyup="propertyvalue(this)"><h3 style="margin-left:100px;">value</h3><input type="text" style="width:100px;margin-left:100px;" id="'+propertyid+'_value" ></td></tr>');
 			$("#propertyname").val("");
 		}
 	});
