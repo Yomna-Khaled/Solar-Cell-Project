37578a8db4d5d9e9f8d74cc1ef37cd34c99df38c
gehad <gehad_933@hotmail.com>
2015-06-08 20:58:49 +0200

    search final

--- GP/app/assets/javascripts/solarpanel.js
+++ GP/app/assets/javascripts/solarpanel.js
@@ -2,10 +2,18 @@ function searchciteria(){
 	var searchciteria_value = $("#searching" ).val();
 	var searchciteria_text = $("#searching option:selected").text();
 	var searchciteria = searchciteria_text.split("_")[2];
-	$('#searchciteria').val('')
+	$('#searchcriteria').val('')
 	if (searchciteria == 'power') {
-		$('#searchciteria').val('power')
+		$('#searchcriteria').val('power')
 	} else{
-		$('#searchciteria').val('serialno')
+		$('#searchcriteria').val('serialno')
+	};
+	var soldornot=$(".target").val();
+	if (soldornot == 'sold' ) {
+		$('#soldornot').val('notnull');
+	} else if (soldornot == 'unsold') {
+		$('#soldornot').val('NULL');
+	} else{
+		$('#soldornot').val('ALL');
 	};
 }
--- GP/app/models/solar_panel.rb
+++ GP/app/models/solar_panel.rb
@@ -5,10 +5,31 @@ class SolarPanel < ActiveRecord::Base
   presence: true
   validates_uniqueness_of :serialNo
   
-  def self.searchserialno(query)
-  	where("serialNo like ?", "%#{query}%") 
+  def self.searchserialno(query,type)
+    if type == 'ALL'
+      where("serialNo like ?", "%#{query}%")
+    elsif type == 'NULL'
+      where("serialNo like ? AND sold_panel_id is NULL", "%#{query}%")
+    else
+      where("serialNo like ? AND sold_panel_id is NOT NULL", "%#{query}%")   
+    end
   end
-  def self.searchpower(query)
-  	where("power like ?", "%#{query}%")
+  def self.searchpower(query,type)
+    # to handle case if the user enter value float (ex: 200.00) 
+    # this value inserted at the data base as (200) only 
+    # so to compare we must remove the values after the point 
+    aftermark=query.split('.')[1].to_i
+    if aftermark > 0
+      query = query
+    else
+      query = query.split('.')[0]
+    end
+     if type == 'ALL'
+      where("power like ?", "%#{query}%")
+    elsif type == 'NULL'
+      where("power like ? AND sold_panel_id is NULL", "%#{query}%")
+    else
+      where("power like ? AND sold_panel_id is NOT NULL", "%#{query}%")   
+    end
   end
 end
