95dcc009ab9fb772a127754f5939bf1b8217c955
gehad <gehad_933@hotmail.com>
2015-06-10 16:52:03 +0200

    history of vendors

--- GP/app/controllers/containers_controller.rb
+++ GP/app/controllers/containers_controller.rb
@@ -42,7 +42,7 @@ class ContainersController < ApplicationController
   def edit
    if logged_in? and current_category.category=="Sales" 
     @vendors = Vendor.all
-    @containervendor = VendorContainer.where("container_id=?",@container.id)
+    @containervendor = VendorContainer.where("container_id=? AND date IS NULL",@container.id)
     @containervendor_sorted = @containervendor.order(updated_at: :desc)
     @vendor_id =  @containervendor_sorted[0].vendor_id
     @flag="edit"
@@ -79,12 +79,18 @@ class ContainersController < ApplicationController
   # PATCH/PUT /containers/1.json
   def update
     @vendoredit_id = params['vendor']; #to get vendor of certain material
-    @vendororiginal_id = VendorContainer.where("container_id=?",@container.id)[0].vendor_id
+    @current_vendor_record = VendorContainer.where("container_id=? AND date IS NULL",@container.id)
+    @vendororiginal_id =@current_vendor_record[0].vendor_id
     respond_to do |format|
       if @container.update(container_params)
         if @vendoredit_id != @vendororiginal_id
-            @vendorcontainer = VendorContainer.new(vendor_id: @vendoredit_id, container_id: @container.id )  
-            @vendorcontainer.save
+        @record_id = @current_vendor_record[0].id
+        # update last record to set end date for last vendor
+        @last_container_vendor = VendorContainer.find_by(id: @record_id)
+        @last_container_vendor.update(date: Date.today)
+        # create new record with the new vendor for this material
+        @vendorcontainer = VendorContainer.new(container_id: @container.id, vendor_id: @vendoredit_id) 
+        @vendorcontainer.save 
         end
         format.html { redirect_to @container, notice: 'Container was successfully updated.' }
         format.json { render :show, status: :ok, location: @container }
--- GP/app/controllers/materials_controller.rb
+++ GP/app/controllers/materials_controller.rb
@@ -51,7 +51,7 @@ class MaterialsController < ApplicationController
       @properties = Property.all
       @material_property = MaterialProperty.all
       # to select the latest vendor for material 
-      @materialvendor = MaterialVendor.where("material_id=?",@material.id)
+      @materialvendor = MaterialVendor.where("material_id=? AND date IS NULL",@material.id)
       @materialvendor_sorted = @materialvendor.order(updated_at: :desc)
       @vendor_id =  @materialvendor_sorted[0].vendor_id
       # to select all properties for material
@@ -93,7 +93,7 @@ class MaterialsController < ApplicationController
             end
           end
         end
-        @materialvendor = MaterialVendor.new(material_id: material_id, vendor_id: @vendor_id, date: Date.today )
+        @materialvendor = MaterialVendor.new(material_id: material_id, vendor_id: @vendor_id)
         @materialvendor.save
         format.html { redirect_to @material}
         format.json { render :show, status: :created, location: @material }
@@ -114,12 +114,18 @@ class MaterialsController < ApplicationController
 def update
   @vendoredit_id = params['vendor']; #to get vendor of certain material 
   @selected_properties = params['propertycheck']; #it is an array of selected properties
-  @vendororiginal_id = MaterialVendor.where("material_id=?",@material.id)[0].vendor_id
-  # render plain: @selected_properties.inspect
+  @current_vendor_record = MaterialVendor.where("material_id=? AND date IS NULL ",@material.id)
+  @vendororiginal_id = @current_vendor_record[0].vendor_id
+
   respond_to do |format|
     if @material.update(material_params)
       if @vendoredit_id != @vendororiginal_id
-        @materialvendor = MaterialVendor.new(material_id: @material.id, vendor_id: @vendoredit_id, date: Date.today )
+        @record_id = @current_vendor_record[0].id
+        # update last record to set end date for last vendor
+        @last_material_vendor = MaterialVendor.find_by(id: @record_id)
+        @last_material_vendor.update(date: Date.today)
+        # create new record with the new vendor for this material
+        @materialvendor = MaterialVendor.new(material_id: @material.id, vendor_id: @vendoredit_id) 
         @materialvendor.save  
       end
       @material_properties_last = MaterialProperty.where("material_id = ? ", @material.id)
