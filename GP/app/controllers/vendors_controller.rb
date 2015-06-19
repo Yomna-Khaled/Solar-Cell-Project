329572e8cfdd3164bd34f41533bb6ef2b7e781cb
mirrete <mertloka@hotmail.com>
2015-06-19 13:59:45 +0200

    shifts

--- GP/app/controllers/vendors_controller.rb
+++ GP/app/controllers/vendors_controller.rb
@@ -83,7 +83,7 @@ class VendorsController < ApplicationController
             @vendors = VendorMachine.all.order(created_at: :desc)
           end 
        end
-      @vendors = @vendors.paginate(:page => params[:page], :per_page => 3)
+      @vendors = @vendors.paginate(:page => params[:page], :per_page => 6)
     else
       render :file => "/public/404.html",:status  => "404"  
     end     
