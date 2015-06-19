971253d9c88deb985e0692a65a4b7fdf07f17ca6
mirrete <mertloka@hotmail.com>
2015-06-19 13:36:16 +0200

    shifts

--- GP/app/controllers/materials_controller.rb
+++ GP/app/controllers/materials_controller.rb
@@ -1,8 +1,12 @@
 class MaterialsController < ApplicationController
-   skip_before_action :verify_authenticity_token
+   
   before_action :set_material, only: [:show, :edit, :update, :destroy]
+
   rescue_from ActiveRecord::RecordNotFound, :with => :render_404
   
+
+  
+  skip_before_action :verify_authenticity_token
   # Render 404 page when record not found
     def render_404      
        render :file => "/public/404.html", :status => 404,:layout => false
